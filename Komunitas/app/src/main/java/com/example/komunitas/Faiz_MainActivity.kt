30bf087d7535a40b82f2618643dd007670544569
FaizDziaulhaq <faiz21248ti@students.nurulfikri.ac.id>
2023-10-11 22:34:12 +0700

    utama

---
+++ Komunitas/app/src/main/java/com/example/komunitas/Faiz_MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.komunitas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Faiz_MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.faiz_activity_main)
+    }
+}
